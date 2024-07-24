f7606e46bf8bf0a04f3b59fc365e892fb048134d
Carlos Rodriguez <carlos.rodriguez@symplifica.com>
2024-07-24 07:11:01 -0500

    Create user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test 'user with a valid email should be valid' do
+    user = User.new(email: 'test@test.org', password_digest: 'test')
+
+    assert user.valid?
+  end
+
+  test 'user with invalid email should be invalid' do
+    user = User.new(email: 'test', password_digest: 'test')
+
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'test')
+
+    assert_not user.valid?
+  end
+end
