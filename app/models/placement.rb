5b64ef2095d81fb29c73b99c62ad6e47fd6b1825
Carlos Rodriguez <carlos.rodriguez@symplifica.com>
2024-07-25 09:15:24 -0500

    Decreases the product quantity by the placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -20,6 +20,12 @@
 #  product_id  (product_id => products.id)
 #
 class Placement < ApplicationRecord
+  after_create :decrement_product_quantity!
+
   belongs_to :order
   belongs_to :product, inverse_of: :placements
+
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
