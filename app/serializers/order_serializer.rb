e12e38490ee876d60662e06e90e167ac191353d3
Carlos Rodriguez <carlos.rodriguez@symplifica.com>
2024-07-24 22:35:07 -0500

    Adds the show action for order

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,24 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  total      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_orders_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class OrderSerializer
   include FastJsonapi::ObjectSerializer
+  attributes :total
   belongs_to :user
   has_many :products
 end
