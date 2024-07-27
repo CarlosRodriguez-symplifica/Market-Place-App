8d301540a4334c0f1e526edfb384879d3a792c97
Carlos Rodriguez <carlos.rodriguez@symplifica.com>
2024-07-27 10:05:13 -0500

    Factorize pagination

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -8,4 +8,15 @@ module Paginable
   def per_page
     (params[:per_page] || 20).to_i
   end
+
+  def get_links_serializer_options links_paths, colletion
+    {
+      links: {
+        first: send(links_paths, page: 1),
+        last: send(links_paths, page: colletion.total_pages),
+        prev: send(links_paths, page: colletion.prev_page),
+        next: send(links_paths, page: colletion.next_page)
+      }
+    }
+  end
 end
